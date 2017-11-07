0638df6b6ab7db2e0f374a267bb5885427d77290
brian leeson <brianeleeson@gmail.com>
2017-11-07 11:39:30 -0800

    init P2. moved P1

---
+++ P2/client.c
@@ -0,0 +1,466 @@
+/*
+ * client.c
+ *
+ *  Created on: Oct 26, 2017
+ *      Author: brian
+ *      Credit:
+ *      	help from: http://www.cs.rpi.edu/~moorthy/Courses/os98/Pgms/socket.html
+ *      	This implementation of a DLL is a modified form of the DLL found at:
+ *      	https://www.tutorialspoint.com/data_structures_algorithms/doubly_linked_list_program_in_c.htm
+ *      Conceptual help from classmates: randy, jack, amie, anisha, kaley and alision
+ *
+ */
+#include <stdio.h>
+#include "duckchat.h"
+#include "raw.h"
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h> // AF_INET and AF_INET6 address families
+#include <sys/un.h>  // AF_UNIX address family. Used for local communication between programs running on the same computer
+#include <arpa/inet.h> // Functions for manipulating numeric IP addresses.
+#include <netdb.h> // Functions for translating protocol names and host names into numeric addresses.
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/select.h>
+
+//this link always point to first Link
+struct node *head = NULL;
+//this link always point to tail Link
+struct node *tail = NULL;
+struct node *current = NULL;
+
+int input_buff_ctr = 0;
+char input_buff[SAY_MAX];
+
+struct node {
+   char data[32];
+
+   struct node *next;
+   struct node *prev;
+};
+
+//is list empty
+int isEmpty() {
+	if (head == NULL)
+			return 1;
+	else
+		return 0;
+}
+
+//display the list in from head to tail
+void displayForward() {
+
+   //start from the beginning
+   struct node *ptr = head;
+
+   //navigate till the end of the list
+   printf("[ ");
+
+   while(ptr != NULL) {
+      printf("data:%s,  ", ptr->data);
+      ptr = ptr->next;
+   }
+
+   printf(" ]\n");
+}
+
+//insert link at the tail location
+void insertTail(char* data) {
+
+   //create a link
+   struct node *link = (struct node*) malloc(sizeof(struct node));
+   strcpy(link->data, data);
+
+   if(isEmpty()) {
+       //make it the head and tail
+	   head = link;
+       tail = link;
+   }
+   else {
+	   //make link a new tail link
+	   tail->next = link;
+
+	   //mark old tail node as prev of new link
+	   link->prev = tail;
+   }
+
+   //point tail to new tail node
+   tail = link;
+}
+
+//remove link at the tail location
+void removeTail() {
+	if(tail == NULL){
+		return;
+	}
+   //save reference to tail link
+   struct node *tempLink = tail;
+
+   //if only one link
+   if(head->next == NULL) {
+      head = NULL;
+   } else {
+      tail->prev->next = NULL;
+   }
+
+   tail = tail->prev;
+
+   free(tempLink);
+}
+
+int contains(char* data){
+	//start from the first link
+	struct node* current = head;
+
+	//if list is empty
+	if(head == NULL) {
+	  return 0;
+	}
+
+	//navigate through list
+	while(strcmp(current->data, data)) {
+		//if it is tail node
+
+		if(current->next == NULL) {
+			return 0;
+		}
+		else {
+			//move to next link
+			current = current->next;
+		}
+	}
+	return 1;
+}
+
+//remove a link with given data
+void remove(char* data) {
+
+   //start from the first link
+   struct node* current = head;
+
+   //if list is empty
+   if(head == NULL) {
+      return;
+   }
+
+   //navigate through list
+   while(strcmp(current->data, data)) {
+      //if it is tail node
+
+      if(current->next == NULL) {
+         return;
+      } else {
+         //move to next link
+         current = current->next;
+      }
+   }
+
+   //found a match, update the link
+   if(current == head) {
+      //change first to point to next link
+      head = head->next;
+   } else {
+      //bypass the current link
+      current->prev->next = current->next;
+   }
+
+   if(current == tail) {
+      //change tail to point to prev link
+      tail = current->prev;
+   } else {
+      current->next->prev = current->prev;
+   }
+   free(current);
+   return;
+}
+
+void destroyDDL(){
+	while(!isEmpty()){
+		removeTail();
+	}
+}
+
+void handleRead(int readResult){
+	if (readResult < 0){
+		printf("ERROR reading from socket\n");
+		exit(0);
+	}
+}
+
+void clear_input_buff(){
+	input_buff_ctr = 0;
+	input_buff[input_buff_ctr] = '\0';
+}
+
+
+int main(int argc, char *argv[]){
+	raw_mode(); //set raw
+	atexit(cooked_mode); //return to cooked on normal exit
+
+	//parse 3 command line arguments. server_socket(address), server_port, username
+	//argc is num args. argv is list of args
+
+	if (argc != 4){
+		printf("Usage: ./client server_socket server_port username\n");
+		return 1;
+	}
+
+	//create socket
+	int sockfd, server_port;
+	struct sockaddr_in serv_addr;
+	struct hostent *server;
+	char username[USERNAME_MAX];
+	char DEFAULT_CHAN[CHANNEL_MAX] = "Common";
+
+	server_port = atoi(argv[2]);
+	strcpy(username, argv[3]);
+
+	if (( sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0){
+		fprintf(stderr, "ERROR - client: can’t open stream socket\n");
+		return 1;
+	}
+	if ((server = gethostbyname(argv[1])) == NULL) {
+		fprintf(stderr, "ERROR - client: no such host\n");
+		return 1;
+	}
+
+	bzero((char *)&serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+	serv_addr.sin_port = htons(server_port);
+
+	//Statically allocate request structs and set codes
+	struct request_login req_login;
+	req_login.req_type = REQ_LOGIN;
+	struct request_logout  req_logout;
+	req_logout.req_type = REQ_LOGOUT;
+	struct request_join req_join;
+	req_join.req_type = REQ_JOIN;
+	struct request_leave req_leave;
+	req_leave.req_type = REQ_LEAVE;
+	struct request_say req_say;
+	req_say.req_type = REQ_SAY;
+	struct request_list req_list;
+	req_list.req_type = REQ_LIST;
+	struct request_who req_who;
+	req_who.req_type = REQ_WHO;
+
+	//init to chat server - login, join common
+	//login
+	req_login.req_type = REQ_LOGIN;
+	strcpy(req_login.req_username, username);
+	sendto(sockfd, &req_login, sizeof(struct request_login), 0, (struct sockaddr*)&serv_addr,  sizeof(serv_addr));
+
+	//join common
+	req_join.req_type = REQ_JOIN;
+	strcpy(req_join.req_channel, DEFAULT_CHAN);
+	sendto(sockfd, &req_join, sizeof(struct request_join), 0, (struct sockaddr*)&serv_addr,  sizeof(serv_addr));
+	insertTail(DEFAULT_CHAN);
+
+	//while logged in, handle user input, handle server messages
+	char current_char;
+	int inChannel = 1;
+	char cur_channel[32];
+	int UDP_MAX_PAC_SIZE = 65507;
+	char buf[UDP_MAX_PAC_SIZE];
+	int logged_in = 1;
+
+	int newline_flag = 0;
+	int msg_flag = 0;
+	input_buff[0] = '\0';
+	struct text* gen_received_struct;
+	struct text_say* t_say = NULL;
+	struct text_list* t_list = NULL;
+	struct text_who* t_who = NULL;
+	struct text_error* t_error = NULL;
+
+    fd_set s_rd;
+
+	strcpy(cur_channel, DEFAULT_CHAN);
+	printf("> ");
+	fflush(stdout);
+	while(logged_in){
+
+		FD_ZERO(&s_rd);
+		FD_SET(sockfd, &s_rd);
+		FD_SET(fileno(stdin), &s_rd);
+
+		//see if any fds are ready
+		select(sockfd+1, &s_rd, NULL, NULL, NULL);
+		// there is a message from the server
+		if (FD_ISSET(0, &s_rd)){
+			//read and print the next char in stdin
+			current_char = (char)fgetc(stdin);
+
+			fflush(stdout);
+
+			if(input_buff_ctr >= (SAY_MAX-1)){ //leave room for '\0'
+
+				while(current_char != '\n'){//while cur char is not '\n'
+					current_char = (char)fgetc(stdin);
+				}
+				printf("\n");
+			}
+			if (current_char == '\n'){
+				printf("%c", current_char);
+				newline_flag = 1;
+				if (!strcmp(input_buff, "/exit")){
+					sendto(sockfd, &req_logout, sizeof(struct request_logout), 0, (struct sockaddr*)&serv_addr,  sizeof(serv_addr));
+					logged_in = 0;
+					newline_flag = 0; //don't print prompt
+				}
+				else if (!strncmp(input_buff, "/join ", 6 * sizeof(char))){
+					inChannel = 1;
+					strcpy(cur_channel, input_buff+6);
+
+					//parse channel name from input_buff
+					strcpy(req_join.req_channel, cur_channel);
+					sendto(sockfd, &req_join, sizeof(struct request_join), 0, (struct sockaddr*)&serv_addr,  sizeof(serv_addr));
+
+					//add to channel list
+					remove(cur_channel); //remove dup
+					insertTail(cur_channel);
+
+					//clear input_buffer
+					clear_input_buff();
+				}
+				else if (!strncmp(input_buff, "/leave ", 7 * sizeof(char))){
+					//parse channel name from input_buff
+					strcpy(req_leave.req_channel, input_buff+7);
+					sendto(sockfd, &req_leave, sizeof(struct request_leave), 0, (struct sockaddr*)&serv_addr,  sizeof(serv_addr));
+
+					remove(input_buff+7);
+
+					//if leaving current channel
+					if(!strcmp(input_buff+7,cur_channel)){
+						inChannel = 0;
+					}
+
+					//clear input_buffer
+					clear_input_buff();
+				}
+				else if (!strncmp(input_buff, "/list", 5 * sizeof(char))){
+					sendto(sockfd, &req_list, sizeof(struct request_list), 0, (struct sockaddr*)&serv_addr,  sizeof(serv_addr));
+					//clear input_buffer
+					clear_input_buff();
+				}
+				else if (!strncmp(input_buff, "/who ", 5 * sizeof(char))){
+					//parse channel name from input_buff
+					strcpy(req_who.req_channel, input_buff+5);
+					sendto(sockfd, &req_who, sizeof(struct request_who), 0, (struct sockaddr*)&serv_addr,  sizeof(serv_addr));
+
+					//clear input_buffer
+					clear_input_buff();
+				}
+				else if (!strncmp(input_buff, "/switch ", 8 * sizeof(char))){
+					//if client is a member of the specified channel
+					if (contains(input_buff+8)){
+						//set active channel to be that channel
+						strcpy(cur_channel, input_buff+8);
+					}
+
+					else{
+						//display error and don't move channel
+						printf("You have not subscribed to channel %s\n", input_buff+8);
+					}
+
+					//clear input_buffer
+					clear_input_buff();
+				}
+				else if(!strncmp(input_buff, "/", sizeof(char))){
+					printf("%s", "*Unknown command\n");
+
+					//clear input_buffer
+					clear_input_buff();
+				}
+				else{ //not a command, must be a message to be sent
+					if(inChannel){
+						req_say.req_type = REQ_SAY;
+
+						//make message from input_buffer
+						strcpy(req_say.req_channel, cur_channel);
+						strcpy(req_say.req_text, input_buff);
+
+						//send message
+						sendto(sockfd, &req_say, sizeof(struct request_say), 0, (struct sockaddr*)&serv_addr,  sizeof(serv_addr));
+					}
+					//clear input_buffer
+					clear_input_buff();
+				}
+				fflush(stdout);
+		}
+		else{
+			printf("%c", current_char);
+			//put the new char in the buff
+			input_buff[input_buff_ctr++] = current_char;
+			input_buff[input_buff_ctr] = '\0';
+		}
+	}
+
+	if (FD_ISSET(sockfd, &s_rd)){
+		msg_flag = 0;
+
+		//print >32 backspaces to clear
+		printf("\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b");
+
+		//read what whole UDP packet
+		bzero(buf, UDP_MAX_PAC_SIZE);
+		handleRead(read(sockfd, buf, UDP_MAX_PAC_SIZE));
+
+		//cast as generic type and get code
+		gen_received_struct = (struct text*)buf;
+
+		//if say
+		if(gen_received_struct->txt_type == 0){
+			t_say = ((struct text_say*) gen_received_struct);
+			printf("[%s][%s]: %s\n", t_say->txt_channel, t_say->txt_username, t_say->txt_text);
+		}
+
+		//if list
+		else if(gen_received_struct->txt_type == 1){
+			t_list = ((struct text_list*) gen_received_struct);
+			printf("Existing channels:\n");
+			int i;
+			for(i = 0; i < t_list->txt_nchannels; i++){
+				printf(" %s\n", (char*)t_list->txt_channels + (i * CHANNEL_MAX));
+			}
+		}
+
+		//if who
+		else if(gen_received_struct->txt_type == 2){
+			t_who = ((struct text_who*) gen_received_struct);
+			printf("Users on channel %s:\n", t_who->txt_channel);
+			int j;
+			for(j=0; j<t_who->txt_nusernames; j++){
+				printf(" %s\n", (char*)t_who->txt_users + (j * CHANNEL_MAX));
+			}
+		}
+
+		//if error
+		else if(gen_received_struct->txt_type == 3){
+			t_error = ((struct text_error*) gen_received_struct);
+			printf("Error: %s\n", t_error->txt_error);
+		}
+
+		//redisplay input buff
+		printf("> %s", input_buff);
+
+		fflush(stdout);
+	}
+
+		//f newline or message, prompt
+		if (newline_flag || msg_flag){
+			printf("> ");
+			newline_flag = 0;
+			msg_flag = 0;
+		}
+		fflush(stdout);
+	}
+
+	destroyDDL();
+	close(sockfd);
+	return 0;
+}
+
+
---
+++ P2/server.c
@@ -0,0 +1,360 @@
+/*
+ * server.c
+ *
+ *  Created on: Oct 26, 2017
+ *      Author: brian
+ *      Credit:
+ *      Conceptual help from classmates: randy, jack, amie, anisha, kaley and alision
+ *      This implementation of a DLL is a *heavily* modified form of the DLL found at:
+ *     	https://www.tutorialspoint.com/data_structures_algorithms/doubly_linked_list_program_in_c.htm
+ */
+
+#include <stdio.h>
+#include "duckchat.h"
+#include "raw.h"
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h> // AF_INET and AF_INET6 address families
+#include <sys/un.h>  // AF_UNIX address family. Used for local communication between programs running on the same computer
+#include <arpa/inet.h> // Functions for manipulating numeric IP addresses.
+#include <netdb.h> // Functions for translating protocol names and host names into numeric addresses.
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/select.h>
+#include "listOfLists.h"
+
+struct node* dll_channels;
+struct node* dll_users;
+
+void handleRead(int readResult){
+	if (readResult < 0){
+		printf("ERROR reading from socket\n");
+		exit(0);
+	}
+}
+
+int main(int argc, char *argv[]){
+	raw_mode(); //set raw
+	atexit(cooked_mode); //return to cooked on normal exit
+
+	if (argc != 3){
+		printf("Usage: ./server domain_name port_num\n");
+		return 1;
+	}
+	//create server and user lists
+	dll_channels = initDLL();
+	dll_users = initDLL();
+
+	//create socket
+	int sockfd, server_port, loggedIn;
+	server_port = atoi(argv[2]);
+	struct sockaddr_in serv_addr;
+	struct hostent *server;
+	int MAX_REQ_SIZE = sizeof(struct request_say); //should be 128
+	char incoming_buff[MAX_REQ_SIZE];
+	socklen_t addrlen = sizeof(serv_addr);
+	int req_type = -1;
+	struct node* currentUserNode;
+
+	//statically allocate request structs
+	struct request* gen_request_struct;
+	struct request_login* r_login = NULL;
+	struct request_join* r_join = NULL;
+	struct request_leave* r_leave = NULL;
+	struct request_say* r_say = NULL;
+	struct request_who* r_who = NULL;
+
+	//statically allocate text structs
+	struct text_say t_say;
+	t_say.txt_type = TXT_SAY;
+	struct text_error t_error;
+	t_error.txt_type = TXT_ERROR;
+
+	if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0){
+			fprintf(stderr, "ERROR - server: can’t open stream socket\n");
+			return 1;
+	}
+	if ((server = gethostbyname(argv[1])) == NULL) {
+		fprintf(stderr, "ERROR - server: no such host\n");
+		return 1;
+	}
+	//bind sockfd
+	bzero((char *)&serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+	serv_addr.sin_port = htons(server_port);
+
+	if(bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0){
+		fprintf(stderr, "ERROR - server: can't bind socket\n");
+		return 1;
+	}
+
+	//while true
+	while(1){
+		//recvfrom sockfd
+		recvfrom(sockfd, incoming_buff, MAX_REQ_SIZE, 0, (struct sockaddr *)&serv_addr, &addrlen);
+
+		//cast generic
+		gen_request_struct = (struct request*) incoming_buff;
+
+		//get type and cast to specific type
+		req_type = gen_request_struct->req_type;
+
+		//set login flag
+		loggedIn = 1;
+		if ((currentUserNode = find_user(dll_users, &serv_addr)) == NULL){
+			loggedIn = 0;
+		}
+
+		switch(req_type){
+			case REQ_LOGIN: { //login
+				r_login = (struct request_login*) gen_request_struct;
+
+				//take action - Add user to user list
+				struct node* tempNode;
+				tempNode = append(r_login->req_username, dll_users, &serv_addr);
+				if(tempNode == NULL){
+					printf("server: error adding to user list\n");
+					break;
+				}
+
+				printf("server: %s logs in\n", r_login->req_username);
+				dll_users->numNodesInList++;
+				break;
+			}
+			case REQ_LOGOUT: { //logout
+				if(!loggedIn){
+					break;
+				}
+				printf("server: %s logs out\n", currentUserNode->data);
+
+				//take action - remove user from user list and every channel they are in. remove empty channels
+				remove_user(dll_users, &serv_addr);
+
+				//remove user from all channels
+				struct node* channelNode;
+				channelNode = dll_channels->next;
+				while(channelNode != NULL){ //remove user if contained in channel
+					remove_user(channelNode->inner, &serv_addr);
+
+					//if channel has no users, remove channel
+					if(channelNode->inner->next == NULL){
+						remove_channel(channelNode->data, dll_channels);
+					}
+					channelNode = channelNode->next;
+				}
+
+				break;
+			}
+			case REQ_JOIN: { //join
+				r_join = (struct request_join*) gen_request_struct;
+				struct node* tempNode;
+				char tempBuff[USERNAME_MAX];
+
+				//if user not logged in
+				if(!loggedIn){
+					strcpy(t_error.txt_error, "Not logged in");
+					sendto(sockfd, &t_error, sizeof(struct text_error), 0, (struct sockaddr*)&serv_addr,  sizeof(serv_addr));
+					break;
+				}
+
+				//get user name of join request
+				strcpy(tempBuff, currentUserNode->data);
+
+				//if channel not created
+				if((tempNode = find_channel(r_join->req_channel, dll_channels)) == NULL){
+					//create channel
+					tempNode = append(r_join->req_channel, dll_channels, NULL);
+				}
+
+				//finally channel is created, append user to it
+				append(tempBuff, tempNode->inner, &serv_addr);
+
+				printf("server: %s joins channel %s\n", tempBuff, tempNode->data);
+				break;
+			}
+			case REQ_LEAVE: { //leave
+				r_leave = (struct request_leave*) gen_request_struct;
+
+				//if user not logged in
+				if(!loggedIn){
+					strcpy(t_error.txt_error, "Not logged in");
+					sendto(sockfd, &t_error, sizeof(struct text_error), 0, (struct sockaddr*)&serv_addr,  sizeof(serv_addr));
+					break;
+				}
+
+				//take action - remove user from channel
+				struct node* channelNode;
+				channelNode = find_channel(r_leave->req_channel, dll_channels);
+
+				//if channel doesn't exist
+				if(channelNode == NULL){
+					//No channel by the name
+					strcpy(t_error.txt_error, "No channel by the name ");
+					strcat(t_error.txt_error, r_leave->req_channel);
+					sendto(sockfd, &t_error, sizeof(struct text_error), 0, (struct sockaddr*)&serv_addr,  sizeof(serv_addr));
+					break;
+				}
+
+				//remove user from channel
+				if(remove_user(channelNode->inner, currentUserNode->serv_addr) == 0){
+					strcpy(t_error.txt_error, "Not a member of channel ");
+					strcat(t_error.txt_error, r_leave->req_channel);
+					sendto(sockfd, &t_error, sizeof(struct text_error), 0, (struct sockaddr*)&serv_addr,  sizeof(serv_addr));
+					break;
+				}
+
+				channelNode = dll_channels->next;
+				//remove empty channels
+				while(channelNode != NULL){
+					//if channel has no users, remove channel
+					if(channelNode->inner->next == NULL){
+						remove_channel(channelNode->data, dll_channels);
+					}
+					channelNode = channelNode->next;
+				}
+				printf("server: %s leaves channel %s\n", currentUserNode->data, r_leave->req_channel);
+
+				break;
+			}
+			case REQ_SAY: { //say
+				r_say = (struct request_say*) gen_request_struct;
+
+				//if user not logged in
+				if(!loggedIn){
+					strcpy(t_error.txt_error, "Not logged in");
+					sendto(sockfd, &t_error, sizeof(struct text_error), 0, (struct sockaddr*)&serv_addr,  sizeof(serv_addr));
+					break;
+				}
+
+				//take action - send message to ever user in channel
+				struct node* channelNode;
+				struct node* userNode;
+
+				channelNode = find_channel(r_say->req_channel, dll_channels);
+				if(channelNode == NULL){
+					printf("server: %s trying to send to users in non-existing channel %s\n", currentUserNode->data, r_say->req_channel);
+					strcpy(t_error.txt_error, "No channel by the name ");
+					strcat(t_error.txt_error, r_say->req_channel);
+					sendto(sockfd, &t_error, sizeof(struct text_error), 0, (struct sockaddr*)&serv_addr,  sizeof(serv_addr));
+					break;
+				}
+
+				//for each user in the channel, send the say msg to that user
+				userNode = channelNode->inner->next; //userNode is first user in channel
+				if(userNode == NULL){ //sending say to empty channel, this should never happen as the channel should be removed when emptied
+					printf("server: %s trying to send to users in an empty channel %s. Should not be possible.\n", currentUserNode->data, r_say->req_channel);
+					strcpy(t_error.txt_error, "No users in channel ");
+					strcat(t_error.txt_error, r_say->req_channel);
+					sendto(sockfd, &t_error, sizeof(struct text_error), 0, (struct sockaddr*)&serv_addr,  sizeof(serv_addr));
+				}
+
+				printf("server: %s sends say message in %s\n",currentUserNode->data ,r_say->req_channel);
+				while(userNode != NULL){ //send say to each user in channel
+					t_say.txt_type = TXT_SAY;
+					strcpy(t_say.txt_channel, r_say->req_channel);
+					strcpy(t_say.txt_username, currentUserNode->data);
+					strcpy(t_say.txt_text, r_say->req_text);
+					sendto(sockfd, &t_say, sizeof(struct text_say), 0, (struct sockaddr*)userNode->serv_addr,  sizeof(serv_addr));
+					userNode = userNode->next;
+				}
+
+				break;
+			}
+			case REQ_LIST: { //list
+				//if user not logged in
+				if(!loggedIn){
+					strcpy(t_error.txt_error, "Not logged in");
+					sendto(sockfd, &t_error, sizeof(struct text_error), 0, (struct sockaddr*)&serv_addr,  sizeof(serv_addr));
+					break;
+				}
+
+				//take action - send a list all channels
+				struct node* currentNode = dll_channels->next;
+				int structSize = sizeof(struct text_list) + (dll_channels->numNodesInList * sizeof(struct channel_info));
+				struct text_list* t_list = (struct text_list*)malloc(structSize);
+				if (t_list == NULL){
+					fprintf(stderr,"failed malloc\n");
+					continue;
+				}
+
+				t_list->txt_type = TXT_LIST;
+				t_list->txt_nchannels = dll_channels->numNodesInList;
+
+				int i = 0;
+				while(currentNode != NULL){
+					strcpy(t_list->txt_channels[i].ch_channel, currentNode->data);
+					currentNode = currentNode->next;
+					i++;
+				}
+
+				//send to whoever just asked for list
+				sendto(sockfd, t_list, structSize, 0, (struct sockaddr*)&serv_addr,  sizeof(serv_addr));
+				free(t_list);
+
+				break;
+			}
+			case REQ_WHO:{ //who
+				r_who = (struct request_who*) gen_request_struct;
+
+				//if user not logged in
+				if(!loggedIn){
+					strcpy(t_error.txt_error, "Not logged in");
+					sendto(sockfd, &t_error, sizeof(struct text_error), 0, (struct sockaddr*)&serv_addr,  sizeof(serv_addr));
+					break;
+				}
+
+				struct node* channelNode;
+				struct node* userNode;
+
+				channelNode = find_channel(r_who->req_channel, dll_channels);
+				if(channelNode == NULL){
+					printf("server: %s trying to list users in non-existing channel %s\n", currentUserNode->data, r_who->req_channel);
+					strcpy(t_error.txt_error, "No channel by the name ");
+					strcat(t_error.txt_error, r_who->req_channel);
+					sendto(sockfd, &t_error, sizeof(struct text_error), 0, (struct sockaddr*)&serv_addr,  sizeof(serv_addr));
+					break;
+				}
+
+				//take action - send channel name and list of every user in that channel
+				int numInChannel = channelNode->inner->numNodesInList;
+				int structSize = sizeof(struct text_who) + (numInChannel * sizeof(struct user_info));
+				struct text_who* t_who = (struct text_who*)malloc(structSize);
+				if (t_who == NULL){
+					fprintf(stderr,"failed malloc\n");
+					continue;
+				}
+
+				//set fields
+				t_who->txt_type = TXT_WHO;
+				strcpy(t_who->txt_channel, r_who->req_channel);
+				t_who->txt_nusernames =numInChannel;
+
+				//populate channel array
+				int i = 0;
+				userNode = channelNode->inner->next;
+				while(userNode != NULL){
+					strcpy(t_who->txt_users[i].us_username, userNode->data);
+					userNode = userNode->next;
+					i++;
+				}
+
+				//send to whoever just asked for who
+				sendto(sockfd, t_who, structSize, 0, (struct sockaddr*)&serv_addr,  sizeof(serv_addr));
+				free(t_who);
+				break;
+			}
+			default:{
+				strcpy(t_error.txt_error, "Server couldn't match message type.");
+				sendto(sockfd, &t_error, sizeof(struct text_error), 0, (struct sockaddr*)&serv_addr,  sizeof(serv_addr));
+				fprintf(stderr, "ERROR - server: no matching req type\n");
+			}
+		}
+	}
+
+	printf("server exiting. This should not normally happen.\n");
+	removeAll(dll_channels);
+	removeAll(dll_users);
+	return 0;
+}
